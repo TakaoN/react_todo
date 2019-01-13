9005e402a5261ac17d839153b084bc2b46bb3231
Takao Nakada <acnakada@gmail.com>
2019-01-14 05:10:01 +0900

    todo完了状態を追加

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ class App extends Component {
     super();
     this.state = {
       todos: [
-        { id: 1, text: "dataを表示する" },
-        { id: 2, text: "簡単な構成をしる" }
+        { id: 1, text: "dataを表示する", isDone: false },
+        { id: 2, text: "簡単な構成をしる", isDone: false }
       ],
       newTodo: { text: '' }
     };
@@ -17,7 +17,7 @@ class App extends Component {
   handleInput = e => {
     const newId = Math.max.apply(null, this.state.todos.map(t => t.id)) + 1;
     this.setState({
-      newTodo: { id: newId, text: e.target.value }
+      newTodo: { id: newId, text: e.target.value, isDone: false }
     });
   };
 
@@ -37,14 +37,35 @@ class App extends Component {
     }));
   };
 
+  toggleCheckBox = item => {
+    const tempTodos = this.state.todos.map(todo => {
+      if(todo === item){
+        todo.isDone = !todo.isDone;
+      }
+      return todo
+    });
+
+    this.setState(({ todos }) => ({
+      todos: tempTodos
+    }));
+  };
+
   render() {
+    const remaining = this.state.todos.filter(function(todo){
+      return !todo.isDone;
+    });
     return (
       <div className="App">
+        <h1>
+          My todos
+          <span>({remaining.length}/{this.state.todos.length})</span>
+        </h1>
         <ul>
           {this.state.todos.map((item, key) =>{
             return(
               <ToDoItem
                 key={key}
+                toggleCheckBox={this.toggleCheckBox.bind(this, item)}
                 text={item.text}
                 deleteTodo={this.deleteTodo.bind(this, item)}
               />
